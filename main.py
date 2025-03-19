2b661c50746e38d1039cc77430295e7e4094fb1b
mateussguerra <mateusguerra92@gmail.com>
2025-03-19 19:12:49 -0300

    adição do que já estava feito

--- main.py
+++ main.py
@@ -10,24 +10,27 @@ def connect_to_database():
             database='lmv'
         )
         if connection.is_connected():
-            print("Connected to MySQL database")
             return connection
-        
     except mysql.connector.Error as err:
-        print(f"Error: {err}")
-        return None
+        return False
 
 def execute_query(connection, query):
     cursor = connection.cursor()
     try:
         cursor.execute(query)
         results = cursor.fetchall()
-        for row in results:
-            print(row)
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-    finally:
+
+        # Lógica das colunas aqui :)
+        columns = cursor.description
+        colums_list = list()
+        for column in columns:
+            colums_list.append(column[0])
+        
         cursor.close()
+        return results, columns
+    except mysql.connector.Error as err:
+        cursor.close()
+        return [], []
 
 
 def main():
@@ -39,13 +42,11 @@ Insira a query desejada para executar: (Para sair, digite 5)
         print(text)
         query = input()
         
-        if query == '5':
-            break
 
         if connection:
             print("-------------------------------------------\nResultado da query:\n-------------------------------------------")
             execute_query(connection, query)
-            connection.close()
+    connection.close()        
 
 if __name__ == "__main__":
     main()
---
+++ teste.py
@@ -0,0 +1,71 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import scrolledtext, messagebox
+from decimal import Decimal
+from main import *
+
+def exibir_tabela(colunas, conteudo):
+    for row in tabela.get_children():
+        tabela.delete(row)
+
+    tabela["columns"] = colunas
+    tabela.column("#0", width=50, anchor="center")
+
+    for coluna in colunas:
+        tabela.heading(coluna, text=coluna)
+        tabela.column(coluna, width=100, anchor="center")
+
+    
+    for i, linha in enumerate(conteudo):
+        tabela.insert("", "end", values=linha)
+
+def call_query():
+    query = text_area.get("1.0", tk.END).strip() 
+    content, columns = execute_query(connection, query)
+
+    if not query:
+        messagebox.showwarning("Aviso", "Não é possível executar uma query vazia :(")
+        return
+    formated_columns = []
+    for column in columns:
+        formated_columns.append(column[0])
+    print(f"Colunas: {columns}")
+    print(f"Colunas formatadas: {formated_columns}")
+    print(f"Conteúdo: {content}")
+    exibir_tabela(formated_columns, content)
+
+connection = connect_to_database()
+
+if not connection:
+    messagebox.showerror("Erro", "Não foi possível conectar ao banco de dados :(")
+    
+root = tk.Tk()
+root.title("Buscador de Dados LMV")
+
+TITTLE_STYLE = {
+    "font": ("Arial", 22, "bold"), 
+    "fg": "blue",                    
+    "bg": "lightgray",               
+    "padx": 10,                      
+    "pady": 5                        
+}
+
+
+titulo = tk.Label(root, text="Interface de Query", **TITTLE_STYLE)
+titulo.pack(fill="x", pady=10)
+
+label_query = tk.Label(root, text="Insira a sua query aqui: :D")
+label_query.pack(pady=5)
+
+text_area = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=50, height=10)
+text_area.pack(pady=5)
+
+botao_executar = tk.Button(root, text="Rodar Query", command=call_query)
+botao_executar.pack(pady=10)
+
+tabela = ttk.Treeview(root)
+tabela.pack(pady=10, fill="both", expand=True)
+
+root.mainloop()
+
+connection.close()
